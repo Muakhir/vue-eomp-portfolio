08802476c7bae1bf8c025524e3756902155d6324
Muakhir <almuakhirjones@gmail.com>
2024-01-10 11:28:28 +0200

    BG/Logo/Fix

--- src/App.vue
+++ src/App.vue
@@ -35,8 +35,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  background-color: #c2b8a9;
+  color: black;
+  background-color: grey;
 }
 
 nav {
