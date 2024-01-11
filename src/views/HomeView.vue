1a2d2451e524f181aad34daeef78c86ad9d73615
Muakhir <almuakhirjones@gmail.com>
2024-01-11 14:30:44 +0200

    Animation/Img/Text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,53 @@
 <template>
-  <div class="home">
-    <div class="row" v-if="fetchAbout">
+  <section class="home">
+    <div class="container-fluid">
+      <div class="row me-5 ms-5">
+        <div class="col typewriter">
+          <h1 class="h1 typing-animation">Welcome to my Portfolio</h1>
+          <h1 class="h1 typing-animation">My name is Al-Mu'akhir Jones</h1>
+          <h1 class="h1 typing-animation">An aspiring Full-Stack Developer</h1>
+        </div>
 
+        <div class="col mt-5">
+          <img class="Himg img-fluid" src="https://iili.io/J7m9yib.jpg" loading="lazy" alt="">
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-
-
 export default {
-  name: 'HomeViewComp',
-  computed:{
-    jobTitle(){
-      return this.$store.state.about
-    }
-    },
-    mounted(){
-      this.$store.dispatch('fetchAbout')
-    }
+
 }
 </script>
+
+<style scoped>
+.Himg {
+  width: 370px;
+  border-radius: 25px;
+}
+
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  20% { border-color: rgba(99, 145, 183, 0); }
+}
+
+.typing-animation {
+  overflow: hidden;
+  white-space: nowrap;
+  animation: typing 3s steps(20, end), blink-caret 0.75s step-end infinite;
+}
+h1{
+  padding: 30px;
+}
+.typewriter{
+  margin-top: 150px;
+}
+</style>
+
