e8863208ba47e8fbc2e02124d92468b55dcbf6ac
Muakhir <almuakhirjones@gmail.com>
2024-01-12 16:48:46 +0200

    updates

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container-fliud bg-light">
+<div class="container bg-light">
 <div class="row">
     <p class="lead fw-bolder d-flex justify-content-center">AL-Mu'akhir Jones Portfolio
         &copy;<span>{{ currYear }}</span>
@@ -25,5 +25,9 @@ p{
 }
 p:hover{
     color: #F6B342;
+}
+.container{
+    border-top-right-radius: 15px;
+    border-top-left-radius: 15px;
 }
 </style>
