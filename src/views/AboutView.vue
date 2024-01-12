544b72ec65f1933ff36e1b4b0bce865c2abf8913
Muakhir <almuakhirjones@gmail.com>
2024-01-12 09:55:40 +0200

    AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,11 +7,23 @@
           {{ title }}
         </p>
      </div>
-      <div class="row Aimg mb-5">
-        <img src="https://iili.io/J76T6uI.md.jpg" loading="lazy" class="Aimg1" alt="Gaming Setup">
-        <img src="https://iili.io/J76A4LX.md.jpg" loading="lazy" class="Aimg1" alt="tech">
-        <img src="https://iili.io/J765JtI.md.jpg" loading="lazy" class="Aimg1" alt="Dev">
-        <img src="https://iili.io/J76AZ74.md.jpg" loading="lazy" class="Aimg1" alt="Dev">
+      <div class="row Rimg">
+        <div class="col Aimg">
+          <img src="https://iili.io/J76T6uI.md.jpg" loading="lazy" class="Aimg2" alt="Gaming Setup">
+
+        </div>
+        <div class="col Aimg">
+          <img src="https://iili.io/J76A4LX.md.jpg" loading="lazy" class="Aimg1" alt="tech">
+
+        </div>
+        <div class="col Aimg">
+          <img src="https://iili.io/J765JtI.md.jpg" loading="lazy" class="Aimg2" alt="Dev">
+
+        </div>
+        <div class="col Aimg">
+          <img src="https://iili.io/J76AZ74.md.jpg" loading="lazy" class="Aimg3" alt="Dev">
+
+        </div>
       </div>
     </div>
   </div>
@@ -32,13 +44,82 @@
 </script>
 
 <style scoped>
-.Aimg{
-  display: flex;
-  flex-wrap: nowrap;
-  width: 380px;
+.Rimg{
+  
 }
 .Aimg1{
+  float: right;
+  border-radius: 45px;
   display: flex;
-  border-radius: 25px;
+  justify-content: center;
+  padding: 20px;
 }
+.Aimg3{
+  float: right;
+  border-radius: 45px;
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+}
+.Aimg2{
+  float: left;
+  margin-left: 20px;
+  margin-top: 25px;
+  width: 490px;
+  height: 280px;
+  border-radius: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+@media (max-width: 720px){
+  .Rimg{
+    margin-left: 70px;
+  }
+  .Aimg1{
+    width: 500px;
+    margin-top: 0;
+    padding: 5px;
+    float: none;
+  }
+  .Aimg2{
+    margin-top: 0;
+    float: none;
+    margin-left: 3px;
+  }
+  .Aimg3{
+    padding: 5px;
+    float: none;
+  }
+
+}
+@media (max-width: 615px){
+  .Rimg{
+    margin-left: 0;
+    margin-right: 5px;
+  }
+  .Aimg{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    margin-bottom: 0;
+  }
+  .Aimg1{
+    width: 250px;
+    padding: 15px;
+  }
+  .Aimg2{
+    height: 150px;
+    width: 250px;
+    padding: 15px;
+    border-radius: 40px;
+  }
+  .Aimg3{
+    width: 250px;
+    padding: 15px;
+  }
+}
+
 </style>
